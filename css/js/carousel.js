4420c0d1af74df619df63730dc56545b237670e6
Moises011 <eriannbel@gmail.com>
2025-10-15 16:44:20 -0300

    .

--- css/js/carousel.js
+++ css/js/carousel.js
@@ -1,27 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const imagenes = [
+    "imagenes/slide1.jpg",
+    "imagenes/slide2.jpg",
+    "imagenes/slide3.jpg"
+  ];
 
-const imagenes = [
-  "imagenes/slide1.jpg",
-  "imagenes/slide2.jpg",
-  "imagenes/slide3.jpg"
-];
+  let index = 0;
+  const img = document.getElementById("carouselImage");
+  const prev = document.getElementById("prevBtn");
+  const next = document.getElementById("nextBtn");
 
-let index = 0;
-const img = document.getElementById('carouselImagenes');
-const prev = document.getElementById('prevBtn');
-const next = document.getElementById('nextBtn');
+  function mostrar() {
+    img.src = imagenes[index];
+  }
 
-function mostrar() {
-  img.src = imagenes[index];
-}
+  prev.addEventListener("click", () => {
+    index = (index - 1 + imagenes.length) % imagenes.length;
+    mostrar();
+  });
 
-prev.addEventListener('click', () => {
-  index = (index - 1 + imagenes.length) % images.length;
+  next.addEventListener("click", () => {
+    index = (index + 1) % imagenes.length;
+    mostrar();
+  });
+
   mostrar();
 });
-
-next.addEventListener('click', () => {
-  index = (index + 1) % imagenes.length;
-  mostrar();
-});
-
-mostrar();
